ed9ab93b51b514f87dd5fe82f7544a89003aa8c7
sonic27289 <sonic27289@gmail.com>
2022-11-22 15:52:38 -0300

    TP04-PDMI6

---
+++ FetchData/FetchData/FetchData/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace FetchData
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Login();
+        }
+
+        public async void Login()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("http://localhost:44322/api/Logins1");
+            var login = JsonConvert.DeserializeObject<List<Login>>(response);
+            ListLogin.ItemsSource = login;
+        }
+    }
+}
